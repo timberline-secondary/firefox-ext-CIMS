c2b4d76f10dc3913edb885b6e7a63a65a0dc13a1
AgeOf <tylerecouture@gmail.com>
2018-01-27 12:15:54 -0800

    working beta

--- content_scripts/importmarks.js
+++ content_scripts/importmarks.js
@@ -2,11 +2,127 @@
  * Created by couture on 26/01/18.
  */
 
-console.log("tests4");;
+/**
+ * Convenience function to help find matching names
+ * @param char
+ */
+String.prototype.a = function () {
+    return this.trim().toUpperCase();
+};
+String.prototype.b = function () {
+    let re = /[^A-Za-z\\s]+/g; // all non letters
+    return this.replace(re, "").toUpperCase();
+};
 
-$('body').css("color", "red");
+(function () {
+    /**
+     * Check and set a global guard variable.
+     * If this content script is injected into the same page again,
+     * it will do nothing next time.
+     */
+    if (window.hasRun) {
+        return;
+    }
+    window.hasRun = true;
 
-$(document).ready(function(){
-  console.log("jquery ready");
-});
+
+    /**
+     * Given a URL to a beast image, remove all existing beasts, then
+     * create and style an IMG node pointing to
+     * that image, then insert the node into the document.
+     */
+    function importMarks() {
+        let jsonMarkData = prompt("Paste JSON Mark Data");
+        // console.log(jsonMarkData);
+        jsonMarkData = jQuery.parseJSON(jsonMarkData);
+        console.log("Data length: " + jsonMarkData.length);
+        // console.log(jsonMarkData);
+        let $rowsNotFound = $();
+
+        let $markstable = $('table#FormContentPlaceHolder_FormContentPlaceHolder_ContentPlaceHolder1_GradeList');
+        //$markstable.css('background-color', 'red');
+        let $rows = $markstable.find('tr').slice(2, -2); //first & last two rows are headers/footers, remove
+
+        $rows.each(function( index ) {
+            let found = false;
+            let $this = $(this);
+            let firstname = $this.find('td:nth-child(2)').text();
+            let lastname = $this.find('td:nth-child(1)').text();
+            let $term2 = $this.find('td > input[name*=IGR02]');
+            let $finrk = $this.find('td > input[name*=IGR08]');
+
+
+            function matchLast(student) {
+                return student['Last Name'].b() === lastname.b();
+            }
+
+            function matchFirst(student) {
+                return student['First Name'].b() === firstname.b();
+            }
+
+            function matchBothNames(student) {
+                return matchLast(student) && matchFirst(student);
+            }
+
+
+            function removeFoundStudent(student) {
+               let index = jsonMarkData.indexOf(student);
+               jsonMarkData.splice(index,1);
+            }
+
+            function processResults(students) {
+                if (students.length == 1) { // exact match found
+                    let mark = Math.min( Math.round(parseFloat(students[0].Mark)), 100);
+                    $finrk.val(mark);
+                    $term2.val(mark);
+                    $this.css('background-color', 'lightgreen');
+                    found = true;
+                    removeFoundStudent(students[0]);
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            if (firstname.length > 1) { // entry isn't blank(ish)... empty cells have a space?
+                // MATCH BOTH NAMES EXACT
+                found = processResults( $.grep(jsonMarkData, matchBothNames) );
+                if (!found) { // student may have netered a nickname, hope for unique last name
+                    found = processResults( $.grep(jsonMarkData, matchLast) );
+                }
+                if (!found) { // last resort... unique first name?
+                    found = processResults( $.grep(jsonMarkData, matchLast) );
+                }
+
+                if (!found) { // no unique exact match yet
+                    $rowsNotFound += $this;
+                    $this.css('background-color','lightcoral');
+                    console.log(`Match not found:  ${firstname} ${lastname}`);
+                }
+            }
+
+
+        });
+
+        console.log("Data length after: " + jsonMarkData.length);
+
+    }
+
+
+    /**
+     * Listen for messages from the background script.
+     * Call "importMarks()" or "reset()".
+     */
+    browser.runtime.onMessage.addListener((message) => {
+        if (message.command === "importMarks") {
+            importMarks();
+        }
+    });
+
+})();
+
+
+
+
 
--- popup/import_data.js
+++ popup/import_data.js
@@ -1,15 +1,65 @@
 /**
  * Created by couture on 26/01/18.
  */
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+    document.addEventListener("click", (e) => {
+
+
+        /**
+         * Insert the page-hiding CSS into the active tab,
+         * then get the beast URL and
+         * send a "beastify" message to the content script in the active tab.
+         */
+        function importMarks(tabs) {
+            browser.tabs.sendMessage(tabs[0].id, {
+                command: "importMarks",
+            });
+        }
+
+        /**
+         * Remove the page-hiding CSS from the active tab,
+         * send a "reset" message to the content script in the active tab.
+         */
+        // function reset(tabs) {
+        //   browser.tabs.removeCSS({code: hidePage}).then(() => {
+        //     browser.tabs.sendMessage(tabs[0].id, {
+        //       command: "reset",
+        //     });
+        //   });
+        // }
+
+        /**
+         * Just log the error to the console.
+         */
+        function reportError(error) {
+            console.error(`Could not beastify: ${error}`);
+        }
+
+        /**
+         * Get the active tab,
+         * then call "beastify()" or "reset()" as appropriate.
+         */
+        if (e.target.classList.contains("import")) {
+            browser.tabs.query({active: true, currentWindow: true})
+                .then(importMarks)
+                .catch(reportError);
+        }
+    });
+}
+
 
 /**
  * There was an error executing the script.
  * Display the popup's error message, and hide the normal UI.
  */
 function reportExecuteScriptError(error) {
-  document.querySelector("#popup-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute beastify content script: ${error.message}`);
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute content script: ${error.message}`);
 }
 
 /**
@@ -18,6 +68,6 @@ function reportExecuteScriptError(error) {
  * If we couldn't inject the script, handle the error.
  */
 browser.tabs.executeScript({file: "/content_scripts/jquery-3.3.1.min.js"});
-browser.tabs.executeScript({file: "/content_scripts/importmarks.js"});
-// .then(listenForClicks)
-// .catch(reportExecuteScriptError);
+browser.tabs.executeScript({file: "/content_scripts/importmarks.js"})
+    .then(listenForClicks)
+    .catch(reportExecuteScriptError);
