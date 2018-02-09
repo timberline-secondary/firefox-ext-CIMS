30484ed1cf80fff77e45ac80b63f5b9b8b972fae
Tylere Couture <tylerecouture@gmail.com>
2018-02-08 23:59:12 -0800

    file picker

--- popup/sd72cims.js
+++ popup/sd72cims.js
@@ -1,6 +1,46 @@
 /**
  * Created by couture on 26/01/18.
  */
+
+
+
+
+// Listen for a file being selected through the file picker
+const inputElement = document.getElementById("input");
+inputElement.addEventListener("change", handlePicked, false);
+
+// Get the image file if it was chosen from the pick list
+function handlePicked() {
+    displayFile(this.files);
+}
+
+
+/*
+Insert the content script and send the image file ObjectURL to the content script using a
+message.
+*/
+function displayFile(fileList) {
+    const imageURL = window.URL.createObjectURL(fileList[0]);
+
+    browser.tabs.executeScript({
+        file: "/content_scripts/attendanceByKeypad.js.js"
+    }).then(messageContent)
+        .catch(reportError);
+
+    function messageContent() {
+        const gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+        gettingActiveTab.then((tabs) => {
+            browser.tabs.sendMessage(tabs[0].id, {fileURL});
+    });
+    }
+
+    function reportError(error) {
+        console.error(`Could not inject content script: ${error}`);
+    }
+}
+
+
+
 /**
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
@@ -9,50 +49,50 @@ function listenForClicks() {
     document.addEventListener("click", (e) => {
 
         function importMarks(tabs) {
-            browser.tabs.sendMessage(tabs[0].id, {
-                command: "importMarks",
-            });
-        }
+        browser.tabs.sendMessage(tabs[0].id, {
+            command: "importMarks",
+        });
+    }
 
-        function attendanceByKeypad(tabs) {
-            browser.tabs.sendMessage(tabs[0].id, {
-                command: "attendanceByKeypad",
-            });
-        }
+    function attendanceByKeypad(tabs) {
+        browser.tabs.sendMessage(tabs[0].id, {
+            command: "attendanceByKeypad",
+        });
+    }
 
-        function attendanceInit(tabs) {
-            browser.tabs.sendMessage(tabs[0].id, {
-                command: "attendanceInit",
-            });
-        }
+    function attendanceInit(tabs) {
+        browser.tabs.sendMessage(tabs[0].id, {
+            command: "attendanceInit",
+        });
+    }
 
-        /**
-         * Just log the error to the console.
-         */
-        function reportError(error) {
-            console.error(`Could not beastify: ${error}`);
-        }
+    /**
+     * Just log the error to the console.
+     */
+    function reportError(error) {
+        console.error(`Could not beastify: ${error}`);
+    }
 
-        /**
-         * Get the active tab,
-         * then call "importMarks()" or "reset()" as appropriate.
-         */
-        if (e.target.classList.contains("import")) {
-            browser.tabs.query({active: true, currentWindow: true})
-                .then(importMarks)
-                .catch(reportError);
-        }
-        if (e.target.classList.contains("attendance")) {
-            browser.tabs.query({active: true, currentWindow: true})
-                .then(attendanceByKeypad)
-                .catch(reportError);
-        }
-        if (e.target.classList.contains("attendanceInit")) {
-            browser.tabs.query({active: true, currentWindow: true})
-                .then(attendanceInit)
-                .catch(reportError);
-        }
-    });
+    /**
+     * Get the active tab,
+     * then call "importMarks()" or "reset()" as appropriate.
+     */
+    if (e.target.classList.contains("import")) {
+        browser.tabs.query({active: true, currentWindow: true})
+            .then(importMarks)
+            .catch(reportError);
+    }
+    if (e.target.classList.contains("attendance")) {
+        browser.tabs.query({active: true, currentWindow: true})
+            .then(attendanceByKeypad)
+            .catch(reportError);
+    }
+    if (e.target.classList.contains("attendanceInit")) {
+        browser.tabs.query({active: true, currentWindow: true})
+            .then(attendanceInit)
+            .catch(reportError);
+    }
+});
 }
 
 
@@ -69,8 +109,10 @@ function reportExecuteScriptError(error) {
  * and add a click handler.
  * If we couldn't inject the script, handle the error.
  */
-browser.tabs.executeScript({file: "/content_scripts/jquery-3.3.1.min.js"});
-browser.tabs.executeScript({file: "/content_scripts/bootstrap/bootstrap.min.js"});
+browser.tabs.executeScript({file: "/content_scripts/jquery-3.3.1.min.js"})
+    .catch(reportExecuteScriptError);
+browser.tabs.executeScript({file: "/content_scripts/bootstrap/bootstrap.min.js"})
+    .catch(reportExecuteScriptError);
 browser.tabs.executeScript({file: "/content_scripts/importmarks.js"})
     .catch(reportExecuteScriptError);
 browser.tabs.executeScript({file: "/content_scripts/attendanceByKeypad.js"})
